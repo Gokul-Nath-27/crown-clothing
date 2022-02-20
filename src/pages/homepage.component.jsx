bef392ed39a668c4a92d81bae4039659383237df
Gokul <gokulnath0827@gmail.com>
2022-02-20 10:26:15 +0530

    Separated homepage elements into components and finished styling all those components

--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -1,41 +1,11 @@
 import React from 'react';
 import './homepage.styles.scss';
+import DirectoryMenu from '../components/directory-menu/directory-menu.component';
 
 const HomePage = () => {
     return (
         <div className="homepage">
-            <div className="directory-menu">
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">Hats</h1>
-                        <span className="subtitle">SHOP NOw</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">Jackets</h1>
-                        <span className="subtitle">SHOP NOw</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">Sneakers</h1>
-                        <span className="subtitle">SHOP NOw</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">Women</h1>
-                        <span className="subtitle">SHOP NOw</span>
-                    </div>
-                </div>
-                <div className="menu-item">
-                    <div className="content">
-                        <h1 className="title">Men</h1>
-                        <span className="subtitle">SHOP NOw</span>
-                    </div>
-                </div>
-            </div>
+            <DirectoryMenu />
         </div>
     )
 }
